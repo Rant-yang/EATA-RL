6d573b5903f3c1e80cdead6f3216a7217e789f62
Yin Tang <tangyin@tom.com>
2023-01-10 09:48:35 +0800

    a bug in main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def evaluate():
 
 
 if __name__ == "__main__":
-    schedule.every().day.at("18:00").do(predict)    # 读数据，预处理，预测，并写入predicted
-    schedule.every().day.at("18:00").do(predict)    # 读数据，预处理，预测，并写入predicted
+    schedule.every().day.at("18:00").do(predict)    # 读preprocessed数据，预处理，预测，并写入predicted
+    schedule.every().day.at("18:00").do(evaluate)    # 读predicted数据，预处理，预测，并写入evaluated
     schedule.every().hour.do(test_webserver)   # 读predicted，进行evaluate，增加一些字段，供visualizer读取并可视化
 
 
